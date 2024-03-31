7e98db12b31742b68ed941f71afb7fdb880874fe
SayanP <leanoholic.trap16@gmail.com>
2024-03-31 19:59:28 +0530

    end of chapter 4

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,6 +20,7 @@ import ColorModeToggle from './ColorModeToggle';
 import { BiUserCheck } from 'react-icons/bi';
 import { toggleFavorites } from '../redux/actions/productActions';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { TbShoppingCart } from 'react-icons/tb';
 
 const Links = [
 	{ name: 'Products', route: '/products' },
@@ -32,6 +33,7 @@ const Header = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const dispatch = useDispatch();
 	const { favoritesToggled } = useSelector((state) => state.product);
+	const { cartItems } = useSelector((state) => state.cart);
 
 	useEffect(() => {}, [favoritesToggled, dispatch]);
 
@@ -45,11 +47,24 @@ const Header = () => {
 						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
 						onClick={isOpen ? onClose : onOpen}
 					/>
+					<IconButton
+						ml='12'
+						position='absolute'
+						icon={<TbShoppingCart size='20px' />}
+						as={ReactLink}
+						to='/cart'
+						variant='ghost'
+					/>
+					{cartItems.length > 0 && (
+						<Text fontWeight='bold' fontStyle='italic' position='absolute' ml='74px' mt='-6' fontSize='sm'>
+							{cartItems.length}
+						</Text>
+					)}
 				</Flex>
 				<HStack spacing='8' alignItems='center'>
 					<Box alignItems='center' display='flex' as={ReactLink} to='/'>
 						<Icon as={BsPhoneFlip} h='6' w='6' color={mode('black', 'yellow.200')} />
-						<Text as='b'>Tech Lines</Text>
+						<Text as='b'>E-commerce</Text>
 					</Box>
 
 					<HStack as='nav' spacing='4' display={{ base: 'none', md: 'flex' }}>
@@ -58,6 +73,14 @@ const Header = () => {
 								<Text fontWeight='medium'>{link.name}</Text>
 							</NavLink>
 						))}
+						<Box>
+							<IconButton icon={<TbShoppingCart size='20px' />} as={ReactLink} to='/cart' variant='ghost' />
+							{cartItems.length > 0 && (
+								<Text fontWeight='bold' fontStyle='italic' position='absolute' ml='26px' mt='-6' fontSize='sm'>
+									{cartItems.length}
+								</Text>
+							)}
+						</Box>
 						<ColorModeToggle />
 						{favoritesToggled ? (
 							<IconButton
